dce1be2df1bf90ac8f289f45319d3185dfd99786
RikBril <riik.b@hotmail.com>
2022-09-14 12:56:37 +0200

    did some further work on the movement logic, which is still a work in progress and untested

--- piecesWhite.py
+++ piecesWhite.py
@@ -2,12 +2,18 @@ class Piece():
     occupied_locations = {}
     piece_types = {"pawn": 1, "rook": 1, "knight": 1, "bischop": 1, "queen": 1, "king": 1}
     
-    def __init__(self, name, location, type):
+    def __init__(self, name, location, is_white):
         self.name = name
         self.location = location
-        self.type = type
+        self.is_white = is_white
         self.has_moved = False
         Piece.occupied_locations[name] = location
+    
+    def showLocation(self):
+        return self.location
+    
+    def setLocation(self, x, y):
+        self.location = [x, y]
         
     def hasMoved(self):
         return self.has_moved
@@ -15,10 +21,16 @@ class Piece():
     def moved(self):
         self.has_moved = True
         
+    def isWhite(self):
+        return self.is_white
+        
 def location_empty(location):
     pass
             
-class Pawn(Piece):
+class PawnWhite(Piece):
+    possible_moves = []
+    
+class PawnBlack(Piece):
     possible_moves = []
     
 class Rook(Piece):
